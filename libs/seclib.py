440202e4b3555c94f47af9bc53acdbbbb100d725
Bryan0x05 <Bryan.b.tait@gmail.com>
2025-04-13 22:15:57 -0400

    Another attempted pyright fix, it really shouldn't be a problem, it can't ever be none but it thinks it can be none

--- libs/seclib.py
+++ libs/seclib.py
@@ -2,7 +2,6 @@ import os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.backends import default_backend
 USERPATH =f"/usrs/{{user}}.pkl"
 class securityManager():
     ''' Security Manager, stores all cryptographic functions'''
@@ -12,10 +11,12 @@ class securityManager():
 
     @staticmethod
     def getSalt( length = 16 ) -> bytes:
+        '''produces a password salt'''
         return os.urandom( length )
     
-    @staticmethod
-    def encryptPwd( pwd: str, salt : bytes, iters: int = 1_000 )->bytes:
+    @staticmethod # NOTE: do not typecast to return bytes, pyright doesn't like it for whatever reasons
+    def encryptPwd( pwd: str, salt : bytes, iters: int = 1_000 ):
+        ''' Encypt passsword, returns the hash in bytes '''
         # Password-based key derivation function 2
         # kdf is the key derivation function
         # TODO: Actually look into if these are good values
@@ -24,17 +25,17 @@ class securityManager():
             length=32,                 # byte length
             salt=salt,                 # salt (noise)
             iterations=iters,          # rounds of hashing
-            backend=default_backend()  # specifically here to see if this gets pywright to stop
         )
         return kdf.derive( pwd.encode() )
-
     
     @staticmethod
     def getUserPath( user):
+        ''' Fills in the blank on the userpath'''
         return USERPATH.format(user=user)
     
     @staticmethod
     def generatePKCKeys() -> tuple[bytes, bytes]:
+        ''' Generate public and private key pair'''
         # TODO: eval if these values make sense
         pubKey = AESGCM.generate_key(bit_length=512)  # AES256 requires 512-bit keys for SIV
         priKey = AESGCM.generate_key(bit_length=512)
@@ -42,13 +43,15 @@ class securityManager():
     
     @staticmethod
     # TODO: IS urandom a secure number generator?
-    # TODO: See if have need of assoicated data
+    # TODO: See if have need of associated data
     def encrypt( key : bytes, data: bytes, nonce : bytes = os.urandom(16), assoicatedData = None ):
+        ''' Encrypt data using the provided key, returns ciphertext and nonce'''
         cipherText =  AESGCM(key).encrypt( nonce , data, assoicatedData )
         return cipherText, nonce
     
     @staticmethod
     def decrypt( key : bytes, data: bytes, nonce: bytes, assoicatedData = None ):
+        ''' Decrypt data with the provided key and nonce'''
         return AESGCM(key).decrypt( nonce, data, assoicatedData )
 
 if __name__ == "__main__":
